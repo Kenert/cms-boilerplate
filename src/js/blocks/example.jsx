66e6b056876f0ec51a9eb40b02f04c7d37620c84
Kenert Kulu <kenertkulu@gmail.com>
2020-09-13 18:21:50 +0300

    Updated example block type to be more advanced

--- src/js/blocks/example.jsx
+++ src/js/blocks/example.jsx
@@ -1,19 +1,36 @@
-import { registerBlockType } from 'wp.blocks';
+const { registerBlockType } = wp.blocks;
+const { RichText } = wp.blockEditor;
 
 const exampleBlock = registerBlockType('rwd/example', {
     title: 'Example Title',
     description: 'Example Description',
     icon: 'format-image',
     category: 'text',
-
-    attributes: {},
-
-    edit() {
-        return <div>Example</div>;
+    supports: {
+        className: false
+    },
+    attributes: {
+        content: {
+            selector: 'div',
+            source: 'children'
+        }
+    },
+    edit(props) {
+        const onChangeContent = (newContent) => {
+            props.setAttributes({ content: newContent });
+        };
+        return (
+            <RichText
+                format='string'
+                onChange={onChangeContent}
+                value={props.attributes.content}
+                placeholder='Example Placeholder'
+            />
+        );
     },
 
-    save() {
-        return <div>Example</div>;
+    save(props) {
+        return <div>{props.attributes.content}</div>;
     }
 });
 
